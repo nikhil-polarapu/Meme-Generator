5622a03d35bf9ad747e9337e7fa73294e54ea176
Nikhil Polarapu <spolarapu@ufl.edu>
2024-08-28 12:27:17 -0400

    Added the project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Meme Generator
+
+This is a React app that fetches random meme images from an API and lets you add text on top of the images to generate memes.
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="troll-face" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React  Project</h4>
+        </header>
+    )
+}
